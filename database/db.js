95fcfacc0c82ce7f1d2cd83dce217ed2b36e1782
Jinxsuxk <marvinjaylorenz@gmail.com>
2025-08-21 14:36:43 +0800

    remove database

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
-const Database = require('better-sqlite3');
-const db = new Database('reminderbot.db');
+// const Database = require('better-sqlite3');
+// const db = new Database('reminderbot.db');
 
-db.prepare(`
-  CREATE TABLE IF NOT EXISTS users (
-    userId TEXT PRIMARY KEY,
-    timezone TEXT
-  )
-`).run();
+// db.prepare(`
+//   CREATE TABLE IF NOT EXISTS users (
+//     userId TEXT PRIMARY KEY,
+//     timezone TEXT
+//   )
+// `).run();
 
-module.exports = db;
+// module.exports = db;
